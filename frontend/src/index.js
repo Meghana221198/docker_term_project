6ba7756b342bc3f91c0e0aa6f029e4b12fe2e03f
Meghana221198 <122943520+Meghana221198@users.noreply.github.com>
2023-12-06 00:42:11 +0000

    commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello World!!! I'm meghana and this is my frontend service!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root')); 
